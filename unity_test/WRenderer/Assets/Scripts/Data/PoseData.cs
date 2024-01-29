9d1c394031c38778ad5b3c117278ff65a8bd5868
Wooram Son <wooram.son@gmail.com>
2024-01-29 16:16:40 +0900

    ground truth loader

---
+++ unity_test/WRenderer/Assets/Scripts/Data/PoseData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class FrameData
+{
+    public int F; // Frame Index
+    public float[][] J; // Joints Data
+}
+
+[Serializable]
+public class Header
+{
+    public List<string> Comment;
+    public List<string> JOINT_NAMES;
+    public int NUM_FRAMES;
+    public int NUM_JOINTS;
+    public List<int> PARENT_IDS;
+}
+
+[Serializable]
+public class PoseData
+{
+    public Header Header;
+    public List<FrameData> Set;
+}
--- unity_test/WRenderer/Assets/Scripts/JointRenderer.cs
+++ unity_test/WRenderer/Assets/Scripts/JointRenderer.cs
@@ -4,47 +4,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Newtonsoft.Json;
+using System;
 
 public class JointRenderer : MonoBehaviour
 {
     // Prefab for the parent of joints
     public GameObject jointsParentDeepRobot;
     public GameObject jointsParentSMPLify;
+    public GameObject jointsParentGroundTruth;
 
     // Prefab for the sphere to represent joints
     public GameObject jointPrefabDeepRobot;
     public GameObject jointPrefabSMPLify;
+    public GameObject jointPrefabGroundTruth;
 
     // Prefab for the cylinder to represent bones
     public GameObject bonePrefabDeepRobot;
     public GameObject bonePrefabSMPLify;
+    public GameObject bonePrefabGroundTruth;
 
     // Joint
     public List<GameObject> jointGameObjectsDeepRobot;
     public List<GameObject> jointGameObjectsSMPLify;
+    public List<GameObject> jointGameObjectsGroundTruth;
 
     // Bone
     public List<GameObject> boneGameObjectsDeepRobot;
     public List<GameObject> boneGameObjectsSMPLify;
+    public List<GameObject> boneGameObjectsGroundTruth;
 
     // Internal use
-    private int frameIndex = 1800;
-    private int frameStartIndex = 1800;
-    private int frameLastIndex = 3600; //2399;  // 3600;
+    private int frameIndex = 2100;//1800;
+    private int frameStartIndex = 2100;//1800;
+    private int frameLastIndex = 2105;//3600; //2399;  // 3600;
 
     private int jointCountDeepRobot = 13;
     private int jointCountSMPLify = 24;
+    private int jointCountGroundTruth = 25;
 
     private float scaleFactorDeepRobot = 0.01f;
 
-    private float alignScaleFactor = 1.0f;
-    private Vector3 alignTransform = new Vector3();
+    private float alignScaleFactorSMPLify = 1.0f;
+    private Vector3 alignTransformSMPLify = new Vector3();
+
+    private float alignScaleFactorGroundTruth = 1.0f;
+    private Vector3 alignTransformGroundTruth = new Vector3();
 
     // The parent index for each joint
     private int[] jointParentIndexDeepRobot = new int[] { -1, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8 };
     //private int[] jointParentIndexSMPLify = new int[] { 3, 0, 0, 6, 1, 2, 9, 4, 5, 12, 7, 8, 15, 12, 12, 24, 13, 14, 16, 17, 18, 19, 20, 21, 24 };
     private int[] jointParentIndexSMPLify = new int[] { 3, 0, 0, 6, 1, 2, 9, 4, 5, 12, 7, 8, 15, 9, 9, -1, 13, 14, 16, 17, 18, 19, 20, 21 };
+    private int[] jointParentIndexGroundTruth = new int[] { 1, -1, 1, 2, 3, 1, 5, 6, 1, 8, 9, 10, 8, 12, 13, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
 
+    // Ground Truth
+    PoseData poseDataGroundTruth;
 
     // Playback
     private float nextActionTime = 0f;
@@ -55,6 +68,10 @@ public class JointRenderer : MonoBehaviour
     void Start()
     {
         InitData();
+
+        string filePath = Directory.GetCurrentDirectory() + "/Data/Frameset_Joints_World3D_opose25_smooth.json";
+        poseDataGroundTruth = LoadGroundTruthPoseData(filePath);
+
         DataLoader(true);
         DataLoader(false);
     }
@@ -76,6 +93,7 @@ public class JointRenderer : MonoBehaviour
 
     private void InitData()
     {
+        // -------------------
         // DeepRobot Joints - Create a list to store the instantiated joint GameObjects
         jointGameObjectsDeepRobot = new List<GameObject>();
 
@@ -93,6 +111,7 @@ public class JointRenderer : MonoBehaviour
             boneGameObjectsDeepRobot.Add(bone);
         }
 
+        // -------------------
         // SMPLify Joints - Create a list to store the instantiated joint GameObjects
         jointGameObjectsSMPLify = new List<GameObject>();
 
@@ -109,12 +128,31 @@ public class JointRenderer : MonoBehaviour
             bone.SetActive(false);
             boneGameObjectsSMPLify.Add(bone);
         }
+
+        // -------------------
+        // Ground Truth Joints - Create a list to store the instantiated joint GameObjects
+        jointGameObjectsGroundTruth = new List<GameObject>();
+
+        // Instantiate joints and store them in the list
+        for (int i = 0; i < jointCountGroundTruth; i++)
+        {
+            Vector3 jointPosition = new Vector3(0, 0, 0);
+            GameObject joint = Instantiate(jointPrefabGroundTruth, jointPosition, Quaternion.identity);
+            joint.transform.SetParent(jointsParentGroundTruth.transform);
+            jointGameObjectsGroundTruth.Add(joint);
+
+            GameObject bone = Instantiate(bonePrefabGroundTruth, jointPosition, Quaternion.identity);
+            bone.transform.SetParent(jointsParentGroundTruth.transform);
+            bone.SetActive(false);
+            boneGameObjectsGroundTruth.Add(bone);
+        }
     }
 
     private void DataLoader(bool calcAlign)
     {
         List<Vector3> jointsListDeepRobot = new List<Vector3>();
         List<Vector3> jointsListSMPLify = new List<Vector3>();
+        List<Vector3> jointsListGroundTruth = new List<Vector3>();
 
         // DeepRobot Joints
         if (jointGameObjectsDeepRobot.Count >= jointCountDeepRobot)
@@ -172,7 +210,7 @@ public class JointRenderer : MonoBehaviour
                 jointsListSMPLify.Add(position);
 
                 // Alignment
-                Vector3 newPosition = (position * alignScaleFactor) + alignTransform;
+                Vector3 newPosition = (position * alignScaleFactorSMPLify) + alignTransformSMPLify;
 
                 // GameObject
                 jointGameObjectsSMPLify[i].transform.localPosition = newPosition * scaleFactorDeepRobot;
@@ -198,11 +236,74 @@ public class JointRenderer : MonoBehaviour
             }
         }
 
+        // Ground Truth Joints
+        if (jointGameObjectsGroundTruth.Count >= jointCountGroundTruth)
+        {
+            FrameData frameData = poseDataGroundTruth.Set[frameIndex - frameStartIndex];
+            var joints = frameData.J;
+
+            // Instantiate joints and store them in the list
+            for (int i = 0; i < joints.Length; i++)
+            {
+                Vector3 position = new Vector3(joints[i][0], -joints[i][1], joints[i][2]);
+                jointsListGroundTruth.Add(position);
+
+                // Alignment
+                Vector3 newPosition = (position * alignScaleFactorGroundTruth) + alignTransformGroundTruth;
+
+                // GameObject
+                jointGameObjectsGroundTruth[i].transform.localPosition = newPosition * scaleFactorDeepRobot;
+            }
+
+            // Bone position
+            for (int i = 0; i < joints.Length; i++)
+            {
+                if (jointParentIndexGroundTruth[i] < 0)
+                    continue;
+
+                GameObject childJoint = jointGameObjectsGroundTruth[i];
+                GameObject parentJoint = jointGameObjectsGroundTruth[jointParentIndexGroundTruth[i]];
+
+                Vector3 direction = childJoint.transform.position - parentJoint.transform.position;
+                float distance = direction.magnitude;
+                direction.Normalize();
+
+                boneGameObjectsGroundTruth[i].transform.position = parentJoint.transform.position + direction * distance * 0.5f;
+                boneGameObjectsGroundTruth[i].transform.up = direction;
+                boneGameObjectsGroundTruth[i].transform.localScale = new Vector3(0.05f, distance * 0.5f, 0.05f);
+                boneGameObjectsGroundTruth[i].SetActive(true);
+            }
+
+        }
+
         // Calculate Alignment
         if (calcAlign == true)
-            (alignScaleFactor, alignTransform) = AdjustScaleAndPosition(jointsListDeepRobot, jointsListSMPLify);
+        {
+            // DeepRobot <-> SMPLify
+            //(alignScaleFactorSMPLify, alignTransformSMPLify) = AdjustScaleAndPosition(jointsListDeepRobot, jointsListSMPLify);
+            (alignScaleFactorSMPLify, alignTransformSMPLify) = AdjustScaleAndPosition(jointsListDeepRobot[1], jointsListDeepRobot[2], jointsListSMPLify[16], jointsListSMPLify[17]);
+
+            // DeepRobot <-> GroundTruth
+            (alignScaleFactorGroundTruth, alignTransformGroundTruth) = AdjustScaleAndPosition(jointsListDeepRobot[1], jointsListDeepRobot[2], jointsListGroundTruth[2], jointsListGroundTruth[5]);
+        }
     }
 
+    private PoseData LoadGroundTruthPoseData(string filePath)
+    {
+        try
+        {
+            string jsonData = File.ReadAllText(filePath);
+            PoseData poseData = JsonConvert.DeserializeObject<PoseData>(jsonData);
+            return poseData;
+        }
+        catch (Exception ex)
+        {
+            UnityEngine.Debug.LogError("Error reading pose data: " + ex.Message);
+            return null;
+        }
+    }
+
+    
     private (float, Vector3) AdjustScaleAndPosition(List<Vector3> jointsListDeepRobot, List<Vector3> jointsListSMPLify)
     {
         // 스케일 계산
@@ -215,12 +316,26 @@ public class JointRenderer : MonoBehaviour
         Vector3 smplifyShoulderCenter = (jointsListSMPLify[16] + jointsListSMPLify[17]) / 2 * scale;
         Vector3 displacement = robotShoulderCenter - smplifyShoulderCenter;
 
-        /*
         // 위치 조정
-        Vector3 robotPoint = jointsListDeepRobot[(int)JOINT_IDX_3D.CAPSKEL_Neck];
-        Vector3 smplifyPoint = jointsListSMPLify[12] * scale;
-        Vector3 displacement = robotPoint - smplifyPoint;
-        */
+        //Vector3 robotPoint = jointsListDeepRobot[(int)JOINT_IDX_3D.CAPSKEL_Neck];
+        //Vector3 smplifyPoint = jointsListSMPLify[12] * scale;
+        //Vector3 displacement = robotPoint - smplifyPoint;
+
+        return (scale, displacement);
+    }
+    
+
+    private (float, Vector3) AdjustScaleAndPosition(Vector3 LShoulderDeepRobot, Vector3 RShoulderDeepRobot, Vector3 LShoulderSMPLify, Vector3 RShoulderSMPLify)
+    {
+        // 스케일 계산
+        float robotDistance = Vector3.Distance(LShoulderDeepRobot, RShoulderDeepRobot);
+        float smplifyDistance = Vector3.Distance(LShoulderSMPLify, RShoulderSMPLify);
+        float scale = robotDistance / smplifyDistance;
+
+        // 위치 조정: 어깨 중심점을 기준으로 조정
+        Vector3 robotShoulderCenter = (LShoulderDeepRobot + RShoulderDeepRobot) / 2;
+        Vector3 smplifyShoulderCenter = (LShoulderSMPLify + RShoulderSMPLify) / 2 * scale;
+        Vector3 displacement = robotShoulderCenter - smplifyShoulderCenter;
 
         return (scale, displacement);
     }
